3b94c59bedaa413ee49a43481ea115e03b5bf4e9
Ankesh Patra <138644314+ankeshpatra@users.noreply.github.com>
2023-07-24 11:35:36 +0530

    Create 15)Program to input the cost and quantity of the product and provide discount as the price range varies.c

---
+++ 15)Program to input the cost and quantity of the product and provide discount as the price range varies.c
@@ -0,0 +1,42 @@
+/*Program to input the cost and quantity of the product and provide discount as the price range varies*/
+
+#include <stdio.h>
+int main()
+{
+    int cost,q,d,a,na;
+    printf("enter the cost of the product:");
+    scanf("%d",&cost);
+    printf("enter the quantity of the product:");
+    scanf("%d",&q);
+    a=cost*q;
+    printf("the amount is Rs.%d",a);
+    if(a<=1000)
+    {
+        d=a*0;
+        na=a-d;
+        printf("\nthe discount offered is Rs.%d",d);
+        printf("\nthe net amount to be paid after discount = Rs.%d",na);
+    }
+    if(a>1000 && a<=2999)
+    {
+        d=0.1*a;
+        na=a-d;
+        printf("\nthe discount offered is Rs.%d",d);
+        printf("\nthe net amount to be paid after discount = Rs.%d",na);
+    }
+    if(a>=3000 && a<=4999)
+    {
+        d=a*0.2;
+        na=a-d;
+        printf("\nthe discount offered is Rs.%d",d);
+        printf("\nthe net amount to be paid after discount = Rs.%d",na);
+    }
+    if(a>=5000)
+    {
+        d=a*0.3;
+        na=a-d;
+        printf("\nthe discount offered is Rs.%d",d);
+        printf("\nthe net amount to be paid after discount = Rs.%d",na);
+    }
+    return 0;
+}
