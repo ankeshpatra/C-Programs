0c6f5c7572d889d046b14464319cf45e09d7ad4d
Ankesh Patra <138644314+ankeshpatra@users.noreply.github.com>
2023-07-26 10:17:52 +0530

    Add files via upload

---
+++ Program to input principle amount and time in years from the user for term deposit in the bank and print the interest.c
@@ -0,0 +1,35 @@
+//Program to input principle amount and time in years from the user for term deposit in the bank and print the interest
+
+#include <stdio.h>
+int main()
+{
+    float rate,pa;
+    int t;
+    printf("enter the principle amount:");
+    scanf("%f",&pa);
+    printf("enter the time in years:");
+    scanf("%d",&t);
+    if(t<=2)
+    {
+        rate=4*pa/100;
+    }
+    else if (t>2 && t<=4)
+    {
+        rate=6*pa/100;
+    }
+    else if (t>4)
+    {
+        rate=7*pa/100;
+    }
+    printf("The interest earned over the amount deposited = Rs.%.2f",rate);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
